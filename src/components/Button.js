fa1128b07efdcdf563d6f6a43c55d7830db924d5
jha-niraj <jhaniraj45@gmail.com>
2024-06-05 00:11:38 +0530

    Added the background color based on card is added to personal bookshelf or not

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 const Button = ({ title, onClick, route, props }) => {
-    title = route === "" ? title = "Add To BookShelf" : route === "personalshelf" ? title = "Remove from Bookshelf" : title;
+    title = route === "" ? title = "Add To My BookShelf" : route === "personalshelf" ? title = "Remove from Bookshelf" : title;
 
     return (
-        <div className={`bg-gray-300 hover:text-white rounded-lg text-center w-full ${route === "personalshelf" ? "hover:bg-red-500 hover:text-black" : "hover:bg-black"} ${props ? "hidden": "block"}`}>
+        <div className={`bg-gray-300 hover:text-white rounded-lg text-center w-full transition-all duration-200 ${route === "personalshelf" ? "hover:bg-red-500 hover:text-black" : "hover:bg-black"} ${props ? "hidden": "block"}`}>
             <button onClick={onClick} className="p-2 text-xl font-bold transition-all">{ title }</button>
         </div>   
     )
--- src/components/Card.js
+++ src/components/Card.js
@@ -7,9 +7,9 @@ const Card = ({ title, edition, onClick, route }) => {
     if(storedPersonalBooks) {
         bookExists = storedPersonalBooks.find((book) => book.title === title);
     }
-    
+
     return (
-        <div className="flex flex-col items-center justify-between w-full gap-5 shadow-2xl rounded-lg p-2">
+        <div className={`flex flex-col items-center justify-between w-full gap-5 shadow-2xl rounded-lg p-2 ${bookExists ? "bg-green-800 text-white" : ""}`}>
             <div className="w-full flex flex-col gap-2">
                 <h1 className="text-xl font-bold">Book Title: <span className="font-medium">{title}</span></h1>
                 <p className="text-xl font-bold">Edition Count: <span className="font-medium">{edition}</span></p>
